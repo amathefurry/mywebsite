7a382124c61ca636eefe7a22472ec4f8d6fc57a2
ama <sveps29@gmail.com>
2026-04-19 14:24:14 +0300

    changed how server.js works, added more functionality to it

--- server/server.js
+++ server/server.js
@@ -7,19 +7,44 @@ const PORT = 3000;
 const HOST = 'localhost';
 const API_ROOT_URL = process.env.API_ROOT_URL;
 const API_KEY = process.env.API_KEY;
-var API_PARAMETERS = 'user.getinfo&user=amathecutefurry'
-
-var API_SERVICE_URL = `${API_ROOT_URL}?method=${API_PARAMETERS}&api_key=${API_KEY}&`
+const USERNAME = 'amathecutefurry';
 
 app.use('/stats', createProxyMiddleware({
-    target: `http://${API_SERVICE_URL}`,
+    target: `http://${API_ROOT_URL}?method=user.getinfo&user=${USERNAME}&api_key=${API_KEY}&/`,
     changeOrigin: true,
     pathRewrite: {
         [`^/stats`]: '',
     },
 }));
+app.use('/recent', createProxyMiddleware({
+    target: `http://${API_ROOT_URL}?method=user.getRecentTracks&user=${USERNAME}&api_key=${API_KEY}&/`,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/recent`]: '',
+    },
+}));
+app.use('/tt', createProxyMiddleware({
+    target: `http://${API_ROOT_URL}?method=user.getTopTracks&user=${USERNAME}&api_key=${API_KEY}&/`,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/tt`]: '',
+    },
+}));
+app.use('/ta', createProxyMiddleware({
+    target: `http://${API_ROOT_URL}?method=user.getTopAlbums&user=${USERNAME}&api_key=${API_KEY}&/`,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/ta`]: '',
+    },
+}));
+app.use('/tar', createProxyMiddleware({
+    target: `http://${API_ROOT_URL}?method=user.getTopArtists&user=${USERNAME}&api_key=${API_KEY}&/`,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/tar`]: '',
+    },
+}));
 
 app.listen(PORT, HOST, () => {
     console.log(`Starting Proxy at ${HOST}:${PORT}`);
-    console.log(`API_URL is  ${API_SERVICE_URL}`);
 });
